c3414504b1d2bdb62511d2dceb284a7af675915c
zegheim <justinarista@gmail.com>
2019-11-27 11:24:11 +0000

    managed to get room ids. adding docs

--- main.py
+++ main.py
@@ -1,18 +1,90 @@
 import re
 import requests
 from bs4 import BeautifulSoup
+from difflib import SequenceMatcher
 
 
 class RoomIdScraper(object):
     def __init__(self):
-        self.url = "https://www.ed.ac.uk/timetabling-examinations/timetabling/room-bookings/bookable-rooms3"
+        self.base_url = "https://www.ed.ac.uk/"
+        self.campuses_url = (
+            self.base_url
+            + "timetabling-examinations/timetabling/room-bookings/bookable-rooms3"
+        )
         self.session = requests.Session()
-        self.room_ids = []
 
-    def get_soup(self, url):
+    def _get_soup(self, url):
         res = self.session.get(url)
         return BeautifulSoup(res.text, "lxml")
 
     def get_campuses(self):
-        res = self.session.get(self.url)
-        soup = BeautifulSoup(res.text, "lxml")
+        """Method to return campus tag objects for further parsing.
+        
+        Returns:
+            A list of all campus tag objects.
+
+        Example:
+            >>> RoomIdScraper().get_campuses()
+            [<a href="/link/to/campus1">Campus 1</a>, <a href="/link/to/campus2">Campus 2</a>]
+
+        """
+        soup = self._get_soup(self.campuses_url)
+        return soup.find("div", class_="content").find_all("a")
+
+    def get_buildings(self, campus):
+        """Method to return all building soup objects in a given campus for further parsing.
+        
+        Args:
+            campus (bs4.element.Tag): campus whose buildings we are interested in.
+        
+        Returns:
+            A list of all building tag objects.
+
+        Example:
+            >>> RoomIdScraper().get_buildings(campus1)
+            [<div class="item-list"> <h3>Building 1</h3>
+             <ul><li class="views-row views-row-1 views-row-odd views-row-first views-row-last">
+             <a href="/room/room_id_1">Room 1</a>
+             <span class="views-field views-field-field-capacity">Capacity: 64</span>
+             <span class="views-field views-field-field-allocated">1. Centrally Allocated Space</span></li>
+             </ul></div>,
+             <div class="item-list"> <h3>Building 2</h3>
+             <ul><li class="views-row views-row-1 views-row-odd views-row-first views-row-last">
+             <a href="/room/room_id_2">Room 2</a>
+             <span class="views-field views-field-field-capacity">Capacity: 32</span>
+             <span class="views-field views-field-field-allocated">1. Centrally Allocated Space</span></li>
+             <li class="views-row views-row-1 views-row-odd views-row-first views-row-last">
+             <a href="/room/room_id_3">Room 3</a>
+             <span class="views-field views-field-field-capacity">Capacity: 16</span>
+             <span class="views-field views-field-field-allocated">1. Centrally Allocated Space</span></li>
+             </ul></div>]
+             
+        """
+        campus_url = campus.get("href")
+        soup = self._get_soup(self.base_url + campus_url)
+        return soup.find_all("div", class_="item-list")
+
+    def get_rooms(self, building):
+        name = building.find("h3").string
+        rooms = {
+            room.get("href").replace("/room/", ""): room.string
+            for room in building.find_all("a")
+        }
+        return name, rooms
+
+
+def main():
+    ris = RoomIdScraper()
+    room_infos = [
+        ris.get_rooms(building)
+        for campus in ris.get_campuses()
+        for building in ris.get_buildings(campus)
+    ]
+
+    from pprint import pprint
+
+    pprint(room_infos)
+
+
+if __name__ == "__main__":
+    main()
