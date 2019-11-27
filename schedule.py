0d6e0f470d073a115659df173596d6735384a0ed
Justin Arista Kasin <j.kasin@ed.ac.uk>
2019-11-27 11:57:50 +0000

    able to retrieve weekly schedule

--- schedule.py
+++ schedule.py
@@ -11,3 +11,19 @@ class TimetableScraper(object):
     def _get_soup(self, url, **kwargs):
         res = self.session.get(url, **kwargs)
         return BeautifulSoup(res.text, "lxml")
+
+    def get_weekly_schedule(self, id, week):
+        query = {"id": id, "week": week}
+        return self._get_soup(self.url, params=query)
+
+
+def main():
+    with open("data/room_ids.txt") as infile:
+        room_ids = json.load(infile)
+
+    tts = TimetableScraper()
+    print(tts.get_weekly_schedule("0208_00_G.8", 1))
+
+
+if __name__ == "__main__":
+    main()
